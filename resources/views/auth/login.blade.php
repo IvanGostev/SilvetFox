32b05301743eaf2da265b510ac4207a2afcab341
IvanGostev <ivangostev07@gmail.com>
2023-09-05 07:25:50 +0300

    Final

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,92 +1,47 @@
-@extends('layouts.main')
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-@section('content')
-    <!-- Banner Start -->
-    <section class="page-banner m-top-20">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <span class="round-shape"></span>
-                    <h2 class="banner-title">Auth / Login</h2>
-                    <div class="bread-crumb"><a href="/">Home</a> / Login</div>
-                </div>
-            </div>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-    </section>
-    <!-- Banner End -->
 
-<section class="m-top-20">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
 
-                            <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Remember Me') }}
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="row mb-0">
-                                <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-dark">
-                                        {{ __('Login') }}
-                                    </button>
-
-                                    @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                                            {{ __('Forgot Your Password?') }}
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
-    </div>
-</section>
 
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+            </label>
+        </div>
 
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
 
-@endsection
+            <x-primary-button class="ml-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,117 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::group(['namespace' => 'App\Http\Controllers\Market', 'prefix' => 'market'], function () {
-    Route::group(['namespace' => 'Product', 'prefix' => 'products'], function () {
-        Route::get('/', 'IndexController')->name('market.product.index');
-        Route::get('/{product}', 'ShowController')->name('market.product.show');
-        Route::group(['namespace' => 'Comment', 'prefix' => 'comment'], function () {
-            Route::post('/', 'StoreController')->name('market.product.comment.store');
-        });
-    });
-    Route::group(['namespace' => 'Store', 'prefix' => 'store'], function () {
-        Route::get('/', 'IndexAllController')->name('market.stores.index');
-        Route::get('/{store}', 'IndexController')->name('market.store.index');
-    });
-    Route::group(['namespace' => 'Category', 'prefix' => 'category'], function () {
-        Route::get('/{category}', 'IndexController')->name('market.category.index');
-    });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
-    Route::group(['namespace' => 'Main', 'prefix' => 'main'], function () {
-        Route::get('/edit', 'EditController')->name('admin.main.edit');
-        Route::patch('/', 'UpdateController')->name('admin.main.update');
-    });
-    Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
-        Route::get('/', 'IndexController')->name('admin.user.index');
-        Route::get('/{user}/edit', 'EditController')->name('admin.user.edit');
-        Route::patch('/{user}', 'UpdateController')->name('admin.user.update');
-        Route::delete('/{user}', 'DestroyController')->name('admin.user.destroy');
-    });
-    Route::group(['namespace' => 'Store', 'prefix' => 'stores'], function () {
-        Route::get('/', 'IndexController')->name('admin.store.index');
-        Route::get('/{store}/edit', 'EditController')->name('admin.store.edit');
-        Route::patch('/{store}', 'UpdateController')->name('admin.store.update');
-        Route::delete('/{store}', 'DestroyController')->name('admin.store.destroy');
-    });
-    Route::group(['namespace' => 'Category', 'prefix' => 'products-categories'], function () {
-        Route::get('/', 'IndexController')->name('admin.category.index');
-        Route::get('/create', 'CreateController')->name('admin.category.create');
-        Route::post('/', 'StoreController')->name('admin.category.store');
-        Route::get('/{category}', 'ShowController')->name('admin.category.show');
-        Route::get('/{category}/edit', 'EditController')->name('admin.category.edit');
-        Route::patch('/{category}', 'UpdateController')->name('admin.category.update');
-        Route::delete('/{category}', 'DestroyController')->name('admin.category.destroy');
-    });
-    Route::group(['namespace' => 'Product', 'prefix' => 'products'], function () {
-        Route::get('/', 'IndexController')->name('admin.product.index');
-        Route::get('/{product}/edit', 'EditController')->name('admin.product.edit');
-        Route::patch('/{product}', 'UpdateController')->name('admin.product.update');
-        Route::delete('/{product}', 'DestroyController')->name('admin.product.destroy');
-    });
-
-});
-Route::group(['namespace' => 'App\Http\Controllers\Store', 'prefix' => 'store'], function () {
-    Route::group(['namespace' => 'Main'], function () {
-        Route::get('/start', 'StartController')->name('store.main.start');
-        Route::post('/open', 'OpenController')->name('store.main.open');
-        Route::get('/create', 'CreateController')->name('store.main.create');
-        Route::post('/', 'StoreController')->name('store.main.store');
-        Route::get('/{store}/edit', 'EditController')->name('store.main.edit');
-        Route::patch('/{store}', 'UpdateController')->name('store.main.update');
-        Route::get('/{store}/main', 'MainController')->name('store.main.main');
-        Route::get('/{store}/active', 'ActiveController')->name('store.main.active');
-//        Route::delete('/{store}', 'DestroyController')->name('store.main.destroy');
-
-    });
-    Route::group(['namespace' => 'Product', 'prefix' => 'product'], function () {
-        Route::get('/create', 'CreateController')->name('store.product.create');
-        Route::post('/', 'StoreController')->name('store.product.store');
-        Route::get('/{product}/edit', 'EditController')->name('store.product.edit');
-        Route::patch('/{product}', 'UpdateController')->name('store.product.update');
-        Route::get('/{product}/active', 'ActiveController')->name('store.product.active');
-        Route::group(['namespace' => 'Image', 'prefix' => 'images'], function () {
-            Route::get('/{product}', 'IndexController')->name('store.product.image.index');
-            Route::get('/{product}/create', 'CreateController')->name('store.product.image.create');
-            Route::post('/', 'StoreController')->name('store.product.image.store');
-            Route::delete('/{image}/update', 'DestroyController')->name('store.product.image.destroy');
-        });
-    });
-});
-
-Route::group(['namespace' => 'App\Http\Controllers\Profile', 'prefix' => 'profile'], function () {
-    Route::group(['namespace' => 'Balance', 'prefix' => 'balance'], function () {
-        Route::get('/', 'IndexController')->name('profile.balance.index');
-        Route::get('/{user}/replenishment', 'ReplenishmentController')->name('profile.balance.replenishment');
-        Route::get('/{user}/withdrawal', 'WithdrawalController')->name('profile.balance.withdrawal');
-        Route::post('/replenishment', 'ReplenishmentStoreController')->name('profile.balance.replenishmentStore');
-        Route::post('/withdrawal', 'WithdrawalStoreController')->name('profile.balance.withdrawalStore');
-    });
-    Route::group(['namespace' => 'Main'], function () {
-        Route::get('/{user}/show', 'ShowController')->name('profile.main.show');
-        Route::get('/edit', 'EditController')->name('profile.main.edit');
-        Route::patch('/update', 'UpdateController')->name('profile.main.update');
-    });
-    Route::group(['namespace' => 'Chat', 'prefix' => 'chats'], function () {
-        Route::get('/', 'IndexController')->name('profile.chat.index');
-        Route::get('/{user}/show', 'ShowController')->name('profile.chat.show');
-        Route::group(['namespace' => 'Message', 'prefix' => 'messages'], function () {
-            Route::post('/{user}', 'StoreController')->name('profile.chat.message.store');
-        });
-    });
-});
-
-
-Route::group(['namespace' => 'App\Http\Controllers\Order', 'prefix' => 'orders'], function () {
-    Route::group(['namespace' => 'Main'], function () {
-        Route::get('/', 'IndexController')->name('order.main.index');
-        Route::get('/create', 'CreateController')->name('order.main.create');
-        Route::post('/', 'StoreController')->name('order.main.store');
-    });
-});
+require __DIR__.'/auth.php';
