7c9452646738ad2efdaf0d72ffa2751b0a79f418
IvanGostev <ivangostev07@gmail.com>
2023-09-26 20:38:46 +0300

    VersionFinalNewUpgrade

--- app/Http/Controllers/Admin/User/DestroyController.php
+++ app/Http/Controllers/Admin/User/DestroyController.php
@@ -22,70 +22,72 @@ class DestroyController extends Controller
 {
     public function __invoke(User $user)
     {
-        $data = Chat::where('first_user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = Chat::where('second_user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = History::where('user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = Message::where('first_user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = Message::where('second_user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = NewMessage::where('first_user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = NewMessage::where('second_user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = PostComment::where('user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $data = ProductComment::where('user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        if (Store::where('user_id', $user->id)->count() > 0) {
-            $store = Store::where('user_id', $user->id)->first();
-            $data = Product::where('store_id', $store->id)->get();
+        if ($user->role <= auth()->user()->role) {
+            $data = Chat::where('first_user_id', $user->id)->get();
             foreach ($data as $temp ) {
-                if ( ProductImage::where('product_id', $temp->id)->count() > 0) {
-                    $images = ProductImage::where('product_id', $temp->id)->get();
-                    foreach ($images as $image) {
-                        $image->delete();
-                    }
-                }
                 $temp->delete();
             }
-            $store->delete();
-        }
-        $data = ReplenishmentForm::where('user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
+            $data = Chat::where('second_user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = History::where('user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = Message::where('first_user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = Message::where('second_user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = NewMessage::where('first_user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = NewMessage::where('second_user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = PostComment::where('user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = ProductComment::where('user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            if (Store::where('user_id', $user->id)->count() > 0) {
+                $store = Store::where('user_id', $user->id)->first();
+                $data = Product::where('store_id', $store->id)->get();
+                foreach ($data as $temp ) {
+                    if ( ProductImage::where('product_id', $temp->id)->count() > 0) {
+                        $images = ProductImage::where('product_id', $temp->id)->get();
+                        foreach ($images as $image) {
+                            $image->delete();
+                        }
+                    }
+                    $temp->delete();
+                }
+                $store->delete();
+            }
+            $data = ReplenishmentForm::where('user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
 
-        $data = WithdrawalForm::where('user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
+            $data = WithdrawalForm::where('user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $data = Banner::where('user_id', $user->id)->get();
+            foreach ($data as $temp ) {
+                $temp->delete();
+            }
+            $user->delete();
         }
-        $data = Banner::where('user_id', $user->id)->get();
-        foreach ($data as $temp ) {
-            $temp->delete();
-        }
-        $user->delete();
         return redirect()->route('admin.user.index');
     }
 }
--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -1,6 +1,9 @@
 @extends('admin.layouts.main')
 @section('content')
-
+    @if($user->role > auth()->user()->role)
+            <?php header("Location: /admin/users");
+            die(); ?>
+    @endif
     <div class="main-content">
 
         <div class="page-content">
@@ -10,30 +13,37 @@
                         <div class="card">
                             <div class="card-body">
                                 <h4 class="card-title">Редактирование пользователя</h4>
-                                <form class="needs-validation" novalidate="" action="{{ route('admin.user.update', $user->id) }}" method="post">
+                                <form class="needs-validation" novalidate=""
+                                      action="{{ route('admin.user.update', $user->id) }}" method="post">
                                     @csrf
                                     @method('patch')
                                     <div class="row">
                                         <div class="col-md-6">
                                             <div class="mb-3">
                                                 <label class="form-label" for="validationCustom01">Имя</label>
-                                                <input type="text" class="form-control" id="validationCustom01" placeholder="First name" name="name" value="{{$user->name}}" required="">
+                                                <input type="text" class="form-control" id="validationCustom01"
+                                                       placeholder="First name" name="name" value="{{$user->name}}"
+                                                       required="">
                                                 <div class="valid-feedback">
                                                 </div>
                                             </div>
                                         </div>
                                         @if(auth()->user()->role == 3)
-                                        <div class="col-md-6">
-                                            <div class="mb-3">
-                                                <label class="form-label" for="validationCustom02">Баланс</label>
-                                                <input type="number" class="form-control" id="validationCustom02" placeholder="Баланс" name="balance" value="{{$user->balance}}" required="">
-                                                <div class="valid-feedback">
-                                                    Looks good!
+                                            <div class="col-md-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label" for="validationCustom02">Баланс</label>
+                                                    <input type="number" class="form-control" id="validationCustom02"
+                                                           placeholder="Баланс" name="balance"
+                                                           value="{{$user->balance}}" required="">
+                                                    <div class="valid-feedback">
+                                                        Looks good!
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
                                         @else
-                                            <input type="number" class="form-control" hidden="hidden" id="validationCustom02" placeholder="Баланс" name="balance" value="{{$user->balance}}" required="">
+                                            <input type="number" class="form-control" hidden="hidden"
+                                                   id="validationCustom02" placeholder="Баланс" name="balance"
+                                                   value="{{$user->balance}}" required="">
                                         @endif
                                     </div>
 
@@ -42,11 +52,19 @@
                                             <div class="mb-3">
                                                 <label class="form-label" for="validationCustom04">Роль</label>
                                                 <select class="form-select" name="role" id="inlineFormSelectPref">
-                                                    <option {{ $user->role == 0 ? 'selected' : '' }}  value="0">Покупатель</option>
-                                                    <option {{ $user->role == 1 ? 'selected' : '' }}  value="1">Продавец</option>
+                                                    <option {{ $user->role == 0 ? 'selected' : '' }}  value="0">
+                                                        Покупатель
+                                                    </option>
+                                                    <option {{ $user->role == 1 ? 'selected' : '' }}  value="1">
+                                                        Продавец
+                                                    </option>
                                                     @if(auth()->user()->role == 3)
-                                                    <option {{ $user->role == 2 ? 'selected' : '' }}  value="2">Модератор</option>
-                                                    <option {{ $user->role == 3 ? 'selected' : '' }}  value="3">Администратор</option>
+                                                        <option {{ $user->role == 2 ? 'selected' : '' }}  value="2">
+                                                            Модератор
+                                                        </option>
+                                                        <option {{ $user->role == 3 ? 'selected' : '' }}  value="3">
+                                                            Администратор
+                                                        </option>
                                                     @endif
                                                 </select>
                                             </div>
